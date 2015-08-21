020f2327027f2aaa5fcd31d8e33fad5f214b322e
Massimo Calderoni <mcalderoni@heighten.com>
2015-08-21 11:41:45 -0700

    Attempted to fix configkv plugin (did not work).
    I tried resolve this bug, but does not work, have to revisit. Saving commit for now.
    The bug is about `default_` properties profiles that contain a property default such as, say, ${project.basedir}, which is convenient to use for absolute paths (user does not have to necessarily put that in settigns.xml).
    If the user does so, that property ends up being listed in the defaults sectino of the output file of this plugin (configkv.properties), but it cannot possibly be a default, because it has an absolute path.
    
    Unfortunately, when i try distunguish between a property that has a token or not, it is too late, because Maven has already resolved it. Need to figure out how to parse the pom and see if the value textually contains a `$` symbol or not.

--- tip/hood/configkv-maven-plugin/src/main/java/com/tip/hood/configkv/ConfigKVMojo.java
+++ tip/hood/configkv-maven-plugin/src/main/java/com/tip/hood/configkv/ConfigKVMojo.java
@@ -54,181 +54,196 @@ import org.codehaus.plexus.util.xml.pull.XmlPullParserException;
 @Mojo(name = "configkv", defaultPhase = LifecyclePhase.PROCESS_SOURCES, aggregator = false)
 public class ConfigKVMojo extends AbstractMojo {
 
-   static final String DEFAULT = "default_";
-   /**
-    * Location of the file relative to the target directory.
-    */
-   @Parameter(defaultValue = "${project.build.directory}", property = "outputDirectory", required = false, readonly = true)
-   private File outputDirectory;
+    static final String DEFAULT = "default_";
+    /**
+     * Location of the file relative to the target directory.
+     */
+    @Parameter(defaultValue = "${project.build.directory}", property = "outputDirectory", required = false, readonly = true)
+    private File outputDirectory;
 
-   /**
-    * The relative path to the file where the user wants to save the configuration db information. This path will be
-    * relative to the target directory. Example of correct file path values: config/mydb.properties, config.db.
-    */
-   @Parameter(defaultValue = "configdkv.properties", property = "file", required = false)
-   private String filepath;
+    /**
+     * The relative path to the file where the user wants to save the configuration db information. This path will be
+     * relative to the target directory. Example of correct file path values: config/mydb.properties, config.db.
+     */
+    @Parameter(defaultValue = "configdkv.properties", property = "file", required = false)
+    private String filepath;
 
-   @Parameter(defaultValue = "${settings}", property = "_settings", required = false, readonly = true)
-   private Settings _settings;
+    @Parameter(defaultValue = "${settings}", property = "_settings", required = false, readonly = true)
+    private Settings _settings;
 
-   /**
-    * The absolute path to a user-provided settings.xml.
-    */
-   @Parameter(property = "settings", required = false)
-   private File settingsXml;
+    /**
+     * The absolute path to a user-provided settings.xml.
+     */
+    @Parameter(property = "settings", required = false)
+    private File settingsXml;
 
-   /**
-    * The absolute path to a user-provided pom.xml.
-    */
-   @Parameter(property = "pom", required = false)
-   private File pomXml;
+    /**
+     * The absolute path to a user-provided pom.xml.
+     */
+    @Parameter(property = "pom", required = false)
+    private File pomXml;
 
-   @Parameter(defaultValue = "${reactorProjects}", property = "reactorProjects", required = false, readonly = true)
-   private List<MavenProject> projects;
+    @Parameter(defaultValue = "${reactorProjects}", property = "reactorProjects", required = false, readonly = true)
+    private List<MavenProject> projects;
 
-   private Model model;
+    private Model model;
 
-   @Override
-   public void execute() throws MojoExecutionException {
-      if (!outputDirectory.exists()) {
-         outputDirectory.mkdirs();
-      }
+    @Override
+    public void execute() throws MojoExecutionException {
+        if (!outputDirectory.exists()) {
+            outputDirectory.mkdirs();
+        }
 
-      //If pom.xml is provided by the user, use that one, else, get it from the topmost
-      //pom in the entire source tree.
-      if (pomXml != null) {
-         try (Reader reader = new FileReader(pomXml)) {
-            MavenXpp3Reader mavenreader = new MavenXpp3Reader();
-            model = mavenreader.read(reader);
-            model.setPomFile(pomXml);
-         } catch (IOException | XmlPullParserException ex) {
-            throw new MojoExecutionException("Could not read provided settings.xml file " + settingsXml, ex);
-         }
-      } else {
-         //Get the top level pom of the entire tree (first one in reactor list)
-         model = projects.get(0).getModel();
-      }
+        //If pom.xml is provided by the user, use that one, else, get it from the topmost
+        //pom in the entire source tree.
+        if (pomXml != null) {
+            try (Reader reader = new FileReader(pomXml)) {
+                MavenXpp3Reader mavenreader = new MavenXpp3Reader();
+                model = mavenreader.read(reader);
+                model.setPomFile(pomXml);
+            } catch (IOException | XmlPullParserException ex) {
+                throw new MojoExecutionException("Could not read provided settings.xml file " + settingsXml, ex);
+            }
+        } else {
+            //Get the top level pom of the entire tree (first one in reactor list)
+            model = projects.get(0).getModel();
+        }
 
-      //If settings.xml is provided by the user, use that one, else the standard one will be used.
-      if (settingsXml != null) {
-         SettingsXpp3Reader s = new SettingsXpp3Reader();
-         try (FileInputStream sFileStream = new FileInputStream(settingsXml)) {
-            _settings = s.read(sFileStream);
-         } catch (IOException | XmlPullParserException ex) {
-            throw new MojoExecutionException("Could not read provided settings.xml file " + settingsXml, ex);
-         }
-      }
+        //If settings.xml is provided by the user, use that one, else the standard one will be used.
+        if (settingsXml != null) {
+            SettingsXpp3Reader s = new SettingsXpp3Reader();
+            try (FileInputStream sFileStream = new FileInputStream(settingsXml)) {
+                _settings = s.read(sFileStream);
+            } catch (IOException | XmlPullParserException ex) {
+                throw new MojoExecutionException("Could not read provided settings.xml file " + settingsXml, ex);
+            }
+        }
 
-      //All properties with defaults are assumed to be in the pom.
-      //It is also assumed that profiles containing defaults
-      //have id that starts with "default_".
-      Properties defaultProps = extractDefaultProps();
+        Properties defaultProps = Properties.Factory.getInstance();
+        Properties nonDefaultProps = Properties.Factory.getInstance();
+        
+        //All properties with defaults are assumed to be in the pom.
+        //It is also assumed that profiles containing defaults
+        //have id that starts with "default_".
+        //Not all properties defined in a "default_" profile are defaults, because they could contain a maven token
+        //such as for example ${project.basedir}. In that case, it is not a default configuration item, it is an 
+        //item that must be edited by the deployer before deploying.
+        extractPropsFromPom(defaultProps, nonDefaultProps);
 
-      //Extract all properties from the given settings.xml. Here is where all config properties
-      //that don't have a reasonable default should be.
-      Properties settingsProps = extractPropsFromSettings();
+        //Extract all properties from the given settings.xml. Here is where all config properties
+        //that don't have a reasonable default should be (it is assumed that there are no sensible defaults in settings.xml).
+        extractPropsFromSettings(nonDefaultProps);
 
-      //Combine the two sets of properties into one file. First write the non-default ones,
-      //then the default ones
-      File outFile = new File(outputDirectory, filepath);
-      String edit_Section = "###############KEY-VALUE STORE OF CONFIGURATION###############\n"
-              + "This file represents all configurable properties in your application, provided\n"
-              + "that you have declared them in your top pom.xml and settings.xml like specified\n"
-              + "in the wiki.\n"
-              + "#######################################################\n\n\n"
-              + "###############EDIT Section###############\n"
-              + "Please note: the deployer should *EDIT* this section manually\n"
-              + "before using the file for replacing tokens\n"
-              + "#######################################################";
-      save(settingsProps, edit_Section, outFile, false);
-      String defaults_Section = "###############DEFAULTS Section###############\n"
-              + "This section contains values that should already be fine for deployment\n"
-              + "The deployer does not necessarily need to change these values\n"
-              + "#######################################################";
-      save(defaultProps, defaults_Section, outFile, true);
+        //Combine the two sets of properties into one file. First write the non-default ones,
+        //then the default ones
+        File outFile = new File(outputDirectory, filepath);
+        String edit_Section = "###############KEY-VALUE STORE OF CONFIGURATION###############\n"
+                + "This file represents all configurable properties in your application, provided\n"
+                + "that you have declared them in your top pom.xml and settings.xml like specified\n"
+                + "in the wiki.\n"
+                + "#######################################################\n\n\n"
+                + "###############EDIT Section###############\n"
+                + "Please note: the deployer should *EDIT* this section manually\n"
+                + "before using the file for replacing tokens\n"
+                + "#######################################################";
+        save(nonDefaultProps, edit_Section, outFile, false);
+        String defaults_Section = "###############DEFAULTS Section###############\n"
+                + "This section contains values that should already be fine for deployment\n"
+                + "The deployer does not necessarily need to change these values\n"
+                + "#######################################################";
+        save(defaultProps, defaults_Section, outFile, true);
 
-      getLog().info("\n\n**********************\n\n Saved Configuration database in file\n"
-              + outFile.getPath() + ".\n\n"
-              + "**********************\n\n");
-   }
+        getLog().info("\n\n**********************\n\n Saved Configuration database in file\n"
+                + outFile.getPath() + ".\n\n"
+                + "**********************\n\n");
+    }
 
-   /**
-    * Saves given properties, appending or not to the given file.
-    *
-    * @param props properties to be saved
-    * @param headerString optional header string
-    * @param outFile required file for properties to be saved into
-    * @param append if appending to the given file or not
-    * @throws MojoExecutionException
-    */
-   private void save(Properties props, String headerString, File outFile, boolean append)
-           throws MojoExecutionException {
-      Objects.requireNonNull(props, "properties argument cannot be null");
-      Objects.requireNonNull(outFile, "input file argument cannot be null");
-      OutputAdapter out = new OutputAdapter(props);
-      OutputFormat format = new ConfigKVFormat(headerString);
-      try (Writer w = new FileWriter(outFile, append)) {
-         out.writeTo(w, format);
-      } catch (IOException ex) {
-         String msg = "Error saving file " + outFile.getPath();
-         getLog().error(msg, ex);
-         throw new MojoExecutionException(msg, ex);
-      }
-   }
+    /**
+     * Saves given properties, appending or not to the given file.
+     *
+     * @param props properties to be saved
+     * @param headerString optional header string
+     * @param outFile required file for properties to be saved into
+     * @param append if appending to the given file or not
+     * @throws MojoExecutionException
+     */
+    private void save(Properties props, String headerString, File outFile, boolean append)
+            throws MojoExecutionException {
+        Objects.requireNonNull(props, "properties argument cannot be null");
+        Objects.requireNonNull(outFile, "input file argument cannot be null");
+        OutputAdapter out = new OutputAdapter(props);
+        OutputFormat format = new ConfigKVFormat(headerString);
+        try (Writer w = new FileWriter(outFile, append)) {
+            out.writeTo(w, format);
+        } catch (IOException ex) {
+            String msg = "Error saving file " + outFile.getPath();
+            getLog().error(msg, ex);
+            throw new MojoExecutionException(msg, ex);
+        }
+    }
 
-   static List<Profile> extractActiveProfiles(Settings settings) {
-      List<Profile> profiles = settings.getProfiles();
-      List<String> activeProfIds = settings.getActiveProfiles();
-      List<Profile> activeProfiles = new ArrayList<>();
-      Set<String> hashSet = new HashSet<>();
-      hashSet.addAll(activeProfIds);
-      for (Profile profile : profiles) {
-         if (hashSet.contains(profile.getId())) {
-            activeProfiles.add(profile);
-         }
-      }
-      return activeProfiles;
-   }
+    static List<Profile> extractActiveProfiles(Settings settings) {
+        List<Profile> profiles = settings.getProfiles();
+        List<String> activeProfIds = settings.getActiveProfiles();
+        List<Profile> activeProfiles = new ArrayList<>();
+        Set<String> hashSet = new HashSet<>();
+        hashSet.addAll(activeProfIds);
+        for (Profile profile : profiles) {
+            if (hashSet.contains(profile.getId())) {
+                activeProfiles.add(profile);
+            }
+        }
+        return activeProfiles;
+    }
 
-   private static List<org.apache.maven.model.Profile> extractDefaultProfiles(
-           List<org.apache.maven.model.Profile> pomProfiles) {
-      List<org.apache.maven.model.Profile> defaultProfiles = new ArrayList<>();
-      for (org.apache.maven.model.Profile profile : pomProfiles) {
-         if (profile.getId().startsWith(DEFAULT)) {
-            defaultProfiles.add(profile);
-         }
-      }
-      return defaultProfiles;
-   }
+    private void extractPropsFromPom(Properties defaultProps, Properties nonDefaultProps) {
+        for (org.apache.maven.model.Profile profile : model.getProfiles()) {
+            //we are interested in only profiles that contain default properties. By convention of this plugin,
+            //it is assumed that all those profiles are named starting with "default_" and contain only one 
+            //property each.
+            if (profile.getId().startsWith(DEFAULT)) {
+                java.util.Properties properties = profile.getProperties();
+                Map.Entry<Object, Object> entry = validateDefaultProfile(properties);
+                String value = (String) entry.getValue();
+                //look at the value of property first. If the property contains a dollar symbol, then it is a property
+                //that has been set among defaults, but still contains a token to be resolved by Maven.
+                //That means that this one property cannot be taken as is by the deployer: the deployer must change it.
+                //So this property goes in the non-defaults section.
+                if (value.contains("$")) {
+                    nonDefaultProps.put((String) entry.getKey(), value);
+                } else {
+                    defaultProps.put((String) entry.getKey(), value);
+                }
 
-   private Properties extractDefaultProps() {
-      List<org.apache.maven.model.Profile> pomProfiles = model.getProfiles();
-      List<org.apache.maven.model.Profile> defaultProfiles = extractDefaultProfiles(pomProfiles);
-      java.util.Properties accProps = new java.util.Properties();
-      for (org.apache.maven.model.Profile profile : defaultProfiles) {
-         java.util.Properties properties = profile.getProperties();
-         accProps.putAll(properties);
-      }
-      Properties props = Properties.Factory.getInstance(accProps);
-      return props;
-   }
+            }
+        }
+    }
 
-   /**
-    * Given the list of profiles in s
-    *
-    * @param profiles
-    * @return
-    */
-   private Properties extractPropsFromSettings() {
-      List<Profile> asettingsProfiles = ConfigKVMojo.extractActiveProfiles(_settings);
-      Properties props = Properties.Factory.getInstance();
-      for (Profile profile : asettingsProfiles) {
-         java.util.Properties properties = profile.getProperties();
-         Set<Map.Entry<Object, Object>> entrySet = properties.entrySet();
-         for (Map.Entry<Object, Object> entry : entrySet) {
-            props.put((String) entry.getKey(), (String) entry.getValue());
-         }
-      }
-      return props;
-   }
+    private Map.Entry<Object, Object> validateDefaultProfile(java.util.Properties properties) throws IllegalArgumentException {
+        String errMesg = "There must be only one property in a default_ profile";
+        Objects.requireNonNull(properties, errMesg);
+        Set<Map.Entry<Object, Object>> entrySet = properties.entrySet();
+        if (entrySet.isEmpty() || entrySet.size() > 1) {
+            throw new IllegalArgumentException(errMesg);
+        }
+        Map.Entry<Object, Object> entry = entrySet.iterator().next();
+        return entry;
+    }
+
+    /**
+     * Given the list of profiles in s
+     *
+     * @param profiles
+     * @return
+     */
+    private void extractPropsFromSettings(Properties props) {
+        List<Profile> asettingsProfiles = ConfigKVMojo.extractActiveProfiles(_settings);
+        for (Profile profile : asettingsProfiles) {
+            java.util.Properties properties = profile.getProperties();
+            Set<Map.Entry<Object, Object>> entrySet = properties.entrySet();
+            for (Map.Entry<Object, Object> entry : entrySet) {
+                props.put((String) entry.getKey(), (String) entry.getValue());
+            }
+        }
+    }
 }
